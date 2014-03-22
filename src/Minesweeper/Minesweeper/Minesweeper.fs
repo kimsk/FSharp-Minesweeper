a5c76d5ca5e5a2f414be11628e500be65df0f767
Karlkim Suwamongkol <karlkim@outlook.com>
2014-03-22 12:48:13 -0500

    Minesweeper board can be created

---
+++ src/Minesweeper/Minesweeper/Minesweeper.fs
@@ -0,0 +1,54 @@
+﻿namespace Minesweeper
+
+
+module Board =
+    open System
+
+    let r = new Random()
+
+    type Tile =
+        | Empty
+        | Mine
+
+    let getMinePositions num gridSize =
+        let rec getRandomPositions (positions:(int*int) list) =
+            let newPos = r.Next(fst(gridSize)), r.Next(snd(gridSize))
+
+            let newPositions = 
+                if positions |> List.exists ((=)newPos) then
+                    positions
+                else
+                    newPos::positions
+
+            if newPositions |> List.length = num then newPositions
+            else getRandomPositions newPositions
+        
+        getRandomPositions []
+
+    let createNewBoard mineNum gridSize =
+        let mines = getMinePositions mineNum gridSize
+        [|
+            for row in 0..fst(gridSize)-1 ->
+                [|
+                    for col in 0..snd(gridSize)-1 ->
+                        if mines |> List.exists ((=)(row,col)) then
+                            Mine
+                        else
+                            Empty
+                |]
+        |]
+
+    let toStr (board:Tile[][]) =
+        let boardSize = (board.Length, board.[0].Length)
+
+        let line = [1..snd(boardSize)] |> List.fold (fun acc _ -> acc + "---+") "+"
+
+        let printTile tile =
+            match tile with
+            | Empty -> "   "
+            | Mine -> " X "
+
+        let getRowStr row = row |> Array.fold (fun acc t -> acc + (printTile t) + "|") "|"
+        let boardStr = board |> Array.fold (fun acc r -> acc + (getRowStr r) + "\r\n") ""
+            
+        line + "\r\n" + boardStr + line + "\r\n"       
--- src/Minesweeper/Minesweeper/Script.fsx
+++ src/Minesweeper/Minesweeper/Script.fsx
@@ -1,2 +1,17 @@
-﻿// Define your library scripting code here
+﻿// Requirement from https://github.com/NashXam/minesweeper
+// A 8x8 grid of tiles, a high score, and a current score.
+// Tile types : 
+//  Empty: No mines
+//  Mine
+//  
+
+let gridSize = 8,8
+let mineNum = 10
+
+#load @"Minesweeper.fs"
+open Minesweeper
+
+let boards = [1..10] |> List.map (fun _ -> Board.createNewBoard mineNum gridSize)
+
+boards |> List.iter (fun b -> printfn "%s" (Board.toStr b))
 
